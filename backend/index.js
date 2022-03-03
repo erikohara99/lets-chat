a80abbadca18d138f418760cb2d72d462dea4748
Erik <slicedblue@gmail.com>
2022-03-03 16:54:50 -0500

    added input field and button to send message, emitters working

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,10 @@ io.on("connection", socket => {
     socket.on("disconnect", () => {
         console.log("[DISCONNECT] Connection terminated");
     })
+
+    socket.on("message", (data) => {
+        console.log(data);
+    })
 })
 
 server.listen(8080, () => {console.log("Server started on port 8080")});
