e4b5f0d77880615a7bdfc364b7ef1077e0998f88
Erik <slicedblue@gmail.com>
2022-03-03 14:43:29 -0500

    added connection and disconnection event handlers for server

--- backend/index.js
+++ backend/index.js
@@ -3,4 +3,12 @@ const server = require("http").createServer(app);
 const sio = require("socket.io");
 const io = new sio.Server(server);
 
+io.on("connection", socket => {
+    console.log("[CONNECT] New connection");
+
+    socket.on("disconnect", () => {
+        console.log("[DISCONNECT] Connection terminated");
+    })
+})
+
 server.listen(8080, () => {console.log("Server started on port 8080")});
