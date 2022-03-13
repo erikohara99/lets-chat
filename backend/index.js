aa960690fc4873176b662400a5ea9dce430c1b02
Erik <slicedblue@gmail.com>
2022-03-13 16:34:56 -0400

    added /name command to allow name change, regex checker. new functions to emit as server for cleaner code. //todo: localStorage name change?

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,35 @@ const server = require("http").createServer(app);
 const sio = require("socket.io");
 const io = new sio.Server(server, {cors: {origin: "*"}});
 
-const emit_message = (event, text, name = "Anonymous") => {
+const emitMessage = (text, name = "Anonymous") => {
     let time = new Date();
     time = `${time.getHours()}:${time.getMinutes() >= 10 ? time.getMinutes() : "0" + time.getMinutes()}`;
-    io.emit(event, {text, time, name});
+
+    io.emit("message", {text, time, name});
+}
+
+const emitMessageServer = (text, id = null) => {
+    let name = "[SERVER]";
+    let time = new Date();
+    time = `${time.getHours()}:${time.getMinutes() >= 10 ? time.getMinutes() : "0" + time.getMinutes()}`;
+
+    if(!id) return io.emit("message", {text, time, name});
+    return io.to(id).emit("message", {text, time, name});
+}
+
+const changeNickname = (socket, nickname) => {
+    let old = socket.nickname;
+    socket.nickname = nickname;
+    emitMessageServer(`'${old}' IS NOW KNOWN AS '${nickname}'.`);
 }
 
 io.on("connection", socket => {
     console.log("[CONNECT] New connection");
-    emit_message("message", "USER HAS CONNECTED", "[SERVER]");
+    socket.nickname = "Anonymous";
+    emitMessageServer(`'${socket.nickname}' HAS JOINED THE ROOM.`);
 
     socket.on("disconnecting", () => {
-        emit_message("message", "USER HAS DISCONNECTED", "[SERVER]");
+        emitMessageServer(`'${socket.nickname}' HAS LEFT THE ROOM.`);
     })
 
     socket.on("disconnect", () => {
@@ -22,7 +39,13 @@ io.on("connection", socket => {
     })
 
     socket.on("send", (text) => {
-        emit_message("message", text);
+        let split = text.split(" ");
+        if(split[0] == "/name"){
+            if(/^[a-zA-Z]{4,12}$/.exec(split[1])) return changeNickname(socket, text.split(" ")[1]);
+            return emitMessageServer("Name must be between 4-12 length, letters only.", socket.id);
+        }
+
+        return emitMessage(text, socket.nickname);
     })
 })
 
